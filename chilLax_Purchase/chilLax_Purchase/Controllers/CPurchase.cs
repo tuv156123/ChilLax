b4c80ab7fb59bd57f12ca15432473c2b2e9f8f50
YuLin <tuv156123@gmail.com>
2023-06-15 15:07:34 +0800

    成功連接WEB

--- chilLax_Purchase/chilLax_Purchase/Controllers/CPurchase.cs
+++ chilLax_Purchase/chilLax_Purchase/Controllers/CPurchase.cs
@@ -1,20 +1,34 @@
 ﻿using chilLax_Purchase.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
+using System.Xml.Linq;
 using static NuGet.Packaging.PackagingConstants;
 
 namespace chilLax_Purchase.Controllers
 {
     public class CPurchase : Controller
     {
-        public IActionResult LIst()
+        public IActionResult List()
         {
             ChilLaxContext db = new ChilLaxContext();
-            var datas = from order in db.ProductOrders
-                         join detail in db.OrderDetails on order.OrderId equals detail.OrderId
-                         select order ;
-           
-            return View(datas);
+
+            List<ProductOrderDetail> productOrderDetails = db.ProductOrders
+                .Join(db.OrderDetails,
+                    po => po.OrderId,
+                    od => od.OrderId,
+                    (po, od) => new ProductOrderDetail
+                    {
+                        ProductOrder = po,
+                        OrderDetail = od
+                    })
+                .GroupBy(item => item.ProductOrder.OrderId)
+                .Select(group => group.FirstOrDefault())
+                .ToList();
+
+            return View(productOrderDetails);
         }
+
+
     }
 }
--- chilLax_Purchase/chilLax_Purchase/Models/ProductOrderDetail.cs
+++ chilLax_Purchase/chilLax_Purchase/Models/ProductOrderDetail.cs
@@ -1,12 +1,49 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace chilLax_Purchase.Models
 {
-	public class ProductOrderDetail
-	{
-		public ProductOrder ProductOrder { get; set; }
-		public OrderDetail OrderDetail { get; set; }
-		[DisplayName("小計")]
-		public int Subtotal { get; set; }
-	}
+    public class ProductOrderDetail
+    {
+        ChilLaxContext db = new ChilLaxContext();
+
+        public ProductOrder ProductOrder { get; set; }
+        public OrderDetail OrderDetail { get; set; }
+
+        [DisplayName("小計")]
+        public int Subtotal => this.OrderDetail.CartProductQuantity * this.ProductPrice;
+        [DisplayName("商品價格")]
+        public int ProductPrice
+        {
+            get
+            {
+                return db.Products
+                    .Where(p => p.ProductId == OrderDetail.ProductId)
+                    .Select(p => p.ProductPrice)
+                    .FirstOrDefault();
+            }
+        }
+        [DisplayName("會員姓名")]
+        public string MemberName
+        {
+            get
+            {
+                return db.Members
+                    .Where(p => p.MemberId == ProductOrder.MemberId)
+                    .Select(p => p.MemberName)
+                    .FirstOrDefault();
+            }
+        }
+        public string ProductName
+        {
+            get
+            {
+                return db.Products
+                    .Where(p => p.ProductId == OrderDetail.ProductId)
+                    .Select(p => p.ProductName)
+                    .FirstOrDefault();
+            }
+        }
+    }
+
 }
